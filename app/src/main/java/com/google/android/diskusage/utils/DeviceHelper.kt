ef5bbd696367eea499bca82fe4687e1773610a03
WhiredPlanck <whiredplanck@outlook.com>
2022-05-02 09:26:23 +0800

    src: optimize how to detect device root status
    Ref: https://github.com/Blankj/AndroidUtilCode/blob/ac872f8fb52d569d252e5fe136b0f7af14798f5a/lib/utilcode/src/main/java/com/blankj/utilcode/util/DeviceUtils.java#L50

--- app/src/main/java/com/google/android/diskusage/utils/DeviceHelper.kt
+++ app/src/main/java/com/google/android/diskusage/utils/DeviceHelper.kt
@@ -3,22 +3,20 @@ package com.google.android.diskusage.utils
 import java.io.File
 
 object DeviceHelper {
-    private val PATH: String? = System.getenv("PATH")
-    private const val PATH_TO_BIN_SU = "/system/bin/su"
-    private const val PATH_TO_XBIN_SU = "/system/xbin/su"
 
     @JvmStatic
     fun isDeviceRooted(): Boolean {
-        if (PATH != null) {
-            val pathsToSearch = PATH.split(":")
-            for (path in pathsToSearch) {
-                if (path.isEmpty()) continue
-                val su = File("${path}/su")
-                if (su.exists() && !su.isDirectory) {
-                    return true
-                }
+        val su = "su"
+        val locations = arrayOf(
+            "/system/bin/", "/system/xbin/", "/sbin/", "/system/sd/xbin/",
+            "/system/bin/failsafe/", "/data/local/xbin/", "/data/local/bin/", "/data/local/",
+            "/system/sbin/", "/usr/bin/", "/vendor/bin/"
+        )
+        for (location in locations) {
+            if (File("${location}${su}").exists()) {
+                return true
             }
         }
-        return File(PATH_TO_BIN_SU).isFile || File(PATH_TO_XBIN_SU).isFile
+        return false
     }
 }
